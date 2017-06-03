2d5f9f28184b7a77c36ccc2e1f7f1123d82bcbbc
mik3ycodez <mjharris0114@gmail.com>
2017-06-03 12:43:47 -0700

    connected objects of tableViewCell and tableView

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by cbeuser on 6/3/17.
+//  Copyright © 2017 CBE User. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var detailsLbl: UILabel!
+    
+
+}
