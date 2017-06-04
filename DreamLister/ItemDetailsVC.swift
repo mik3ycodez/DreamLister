7ff8ccc1d1e7fa591585b024afe75658f2a0d62b
mik3ycodez <mjharris0114@gmail.com>
2017-06-04 12:34:03 -0700

    connected ItemDetailsVC Outlets and setup fetchRequest for stores with test stores

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -7,15 +7,83 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MIKE: This has been loaded once already.
+        //generateTestStores()
+        
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+        getStores()
+        
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //update when selected
+    }
+    
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            let err = error as NSError
+            print("\(err)")
+        }
+    }
+    
+    
+    
+    
+    func generateTestStores() {
+        let store = Store(context: context)
+        store.name = "Best Buy"
+        let store2 = Store(context: context)
+        store2.name = "Tesla Dealership"
+        let store3 = Store(context: context)
+        store3.name = "Frys Electronics"
+        let store4 = Store(context: context)
+        store4.name = "Target"
+        let store5 = Store(context: context)
+        store5.name = "Amazon"
+        let store6 = Store(context: context)
+        store6.name = "K Mart"
+        
+        ad.saveContext()
         
     }
 
